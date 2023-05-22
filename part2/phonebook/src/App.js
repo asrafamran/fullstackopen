d706d0f2e076ab94838cb056e3e6bfa69b0d6b64
Asyraaf Amran <asrafamran06@gmail.com>
2023-05-22 22:08:07 +0800

    p2 2.16-2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import personService from "./services/persons";
 
 const Filter = ({ setSearch }) => {
   return (
@@ -26,7 +28,38 @@ const PersonForm = ({ handleName, handleNumber, AddName }) => {
   );
 };
 
-const Person = ({ persons, search }) => {
+const Notification = ({ name, error }) => {
+  if (name === null) {
+    return null;
+  }
+
+  if (error) {
+    return (
+      <div className="border-delete">
+        <h2 className="text_error">
+          Information of {name} has already been removed from server
+        </h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="border_added">
+      <h2 className="text_added">Added {name}</h2>
+    </div>
+  );
+};
+
+const Person = ({ persons, search, setPersons, setName }) => {
+  const handleDelete = (person, id) => {
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      personService.delete(id);
+      const newPersons = persons.filter((person) => person.id !== id);
+      setPersons(newPersons);
+      setName(null);
+    }
+  };
+
   return (
     <>
       {persons
@@ -35,25 +68,31 @@ const Person = ({ persons, search }) => {
             ? person
             : person.name.toLowerCase().includes(search);
         })
-        .map((person, i) => (
-          <p key={i}>
-            {person.name} {person.number}
-          </p>
+        .map((person) => (
+          <div key={person.id}>
+            <p>
+              {person.name} {person.number}
+              <button onClick={() => handleDelete(person, person.id)}>
+                delete
+              </button>
+            </p>
+          </div>
         ))}
     </>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [name, setName] = useState(null);
   const [number, setNumber] = useState(null);
   const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    personService.getAll().then((res) => setPersons(persons.concat(res.data)));
+  }, []);
 
   const handleName = (e) => {
     setNewName(e.target.value);
@@ -66,20 +105,50 @@ const App = () => {
   const AddName = (e) => {
     e.preventDefault();
     let data = {
+      id: persons.length + 1,
       name: newName,
       number: number,
     };
 
     if (!persons.find((person) => person.name === newName)) {
+      setError(false);
+      setName(newName);
+      personService.create(data);
       setPersons(persons.concat(data));
     } else {
-      alert(`${newName} is already added to phonebook`);
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const newPerson = persons.find((p) => p.name === newName);
+        const changedPerson = { ...newPerson, number: number };
+        personService
+          .update(newPerson.id, changedPerson)
+          .then(() => {
+            const updatedPersons = persons.map((p) => {
+              if (newPerson.id == p.id) {
+                return { ...p, number: number };
+              }
+              return p;
+            });
+            setPersons(updatedPersons);
+          })
+          .catch((e) => {
+            console.log();
+            const newPersons = persons.filter((person) => person.id !== newPerson.id);
+            setPersons(newPersons);
+            setName(newName);
+            setError(true);
+          });
+      }
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification name={name} error={error} />
       <Filter setSearch={setSearch} />
       <h2>add a new</h2>
       <PersonForm
@@ -88,7 +157,12 @@ const App = () => {
         AddName={AddName}
       />
       <h2>Numbers</h2>
-      <Person persons={persons} search={search} />
+      <Person
+        persons={persons}
+        search={search}
+        setPersons={setPersons}
+        setName={setName}
+      />
     </div>
   );
 };
